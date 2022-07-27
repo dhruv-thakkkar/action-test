736f30c2746604c05613642df1204dba5e6bbc14
unknown <dnt1997@gmail.com>
2022-07-27 21:58:45 +0530

    lint fix

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = 3000;
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
-const hello = "12312";
+//const hello = "12312";
 app.listen(port, () => {
   console.log(`Example app listenisng on1 p21ort ${port}`);
 });
