db1d54ba41c7867d9e1910e07a61e1a0c49257b0
unknown <dnt1997@gmail.com>
2022-07-27 21:46:45 +0530

    eslint istalled.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,13 @@
 module.exports = {
-  env: {
-    browser: true,
-    commonjs: true,
-    es2021: true,
-  },
-  extends: ["eslint:recommended", "prettier"],
-  parserOptions: {
-    ecmaVersion: "latest",
-  },
-  rules: {},
-};
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es2021": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": "latest"
+    },
+    "rules": {
+    }
+}
